95cfab0c2d31466f04d722a1aaf8cff44d2386eb
Munckenh <76769232+Munckenh@users.noreply.github.com>
2026-05-13 13:57:06 +0800

    chore: update version bump logic

--- version-bump.mjs
+++ version-bump.mjs
@@ -9,6 +9,8 @@ manifest.version = targetVersion;
 writeFileSync('manifest.json', JSON.stringify(manifest, null, '\t'));
 
 // Update versions.json with target version and minAppVersion from manifest.json
-let versions = JSON.parse(readFileSync('versions.json', 'utf8'));
-versions[targetVersion] = minAppVersion;
-writeFileSync('versions.json', JSON.stringify(versions, null, '\t'));
+const versions = JSON.parse(readFileSync('versions.json', 'utf8'));
+if (!Object.values(versions).includes(minAppVersion)) {
+    versions[targetVersion] = minAppVersion;
+    writeFileSync('versions.json', JSON.stringify(versions, null, '\t'));
+}
